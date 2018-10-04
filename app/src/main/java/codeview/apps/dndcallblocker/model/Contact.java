c23f1f102b9363e7e4c6b166011bf8b2a82112a5
Wijay Sharma <thewijaysharma@gmail.com>
2018-10-04 23:04:54 +0530

    contact model added

---
+++ app/src/main/java/codeview/apps/dndcallblocker/model/Contact.java
@@ -0,0 +1,27 @@
+package codeview.apps.dndcallblocker.model;
+
+/**
+ * Created by Wijay on 4/10/2018.
+ */
+
+public class Contact {
+
+    private String name;
+    private String phone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
